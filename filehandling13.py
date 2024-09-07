f6ba01340b3d49a0bfd7e7360081d1210b21887d
AlexFairy <AlexM121687@gmail.com>
2024-09-06 20:58:54 -0700

    updated HW

--- filehandling13.py
+++ filehandling13.py
@@ -2,14 +2,15 @@
 # The script should correctly list all files and subdirectories in the specified directory. 
 # Handle exceptions for invalid paths or inaccessible directories.
 
+#My study notes included to help my understand the program when I get back to overlooking the assignment again
 import os
 
 def list_directory_contents(path='.'):
   try:
-    for x in os.listdir(path):
-      all_files = os.path.join(path, x)
-      if os.path.isdir(all_files):
-        list_directory_contents(all_files)
+    for x in os.listdir(path): #Note: OS.LISTDIR() for listing contents of a directory
+      all_files = os.path.join(path, x) #Note: Join gets iterables and places them in a string
+      if os.path.isdir(all_files): #Note: OS.PATH.ISDIR() for a directory
+        list_directory_contents(all_files) #the function/parameter
       else:
         print(all_files)
   except PermissionError:
@@ -17,7 +18,7 @@ def list_directory_contents(path='.'):
   except Exception as e:
     print(f"A general error occured: {e}")
 
-specific_path = './'
+specific_path = './' #Apporpiate format for file reading. 
 list_directory_contents(specific_path)
   
 
